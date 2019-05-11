7af7b347dca3aa4bbae6894868dad5da806faafc
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-11 14:31:45 -0700

    en_manual/squareSum10.cpp: added rangeNamespace.hpp

---
+++ en_manual/rangeNamespace.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// define common nampespace for ranges
+
+namespace std {
+    namespace view = ranges::view;
+    namespace ranges = ranges;
+}
+
+namespace rng = std::ranges;
--- en_manual/squareSum10.cpp
+++ en_manual/squareSum10.cpp
@@ -1,13 +1,12 @@
 
 #include <range/v3/all.hpp> // get everything
 
+#include "rangeNamespace.hpp"
 #include "printUtil.hpp"
+
 
 int main ()
 {
-
-    //using namespace ranges;
-    namespace rng = ranges;
 
     int sum = rng::accumulate(rng::view::ints(1, rng::unreachable)
                        | rng::view::transform([](int i){return i*i;})
