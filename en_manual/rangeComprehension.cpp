2d2f24c16ffebd7c69d52ab2009854fb9b28cf6d
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-11 19:20:38 -0700

    en_manual/rangeComprehension.cpp: new file

---
+++ en_manual/rangeComprehension.cpp
@@ -0,0 +1,23 @@
+/*
+    Generate a sequence on the fly with range comprehension and initialize a vector with it.
+    https://ericniebler.github.io/range-v3/
+*/
+
+#include <ranges>
+
+#include "rangeNamespace.hpp"
+#include "printUtil.hpp"
+
+namespace view = std::view;
+
+int main ()
+{
+
+    std::vector<int> vi =
+        view::for_each(view::ints(1,10), [](int i){
+            return rng::yield_from(view::repeat_n(i,i));
+        });
+
+    printRng (vi);
+    // vi == {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,...}
+}
