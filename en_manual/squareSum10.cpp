7e29dbd716455622d0d7510276857c5b4fdbb924
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-11 19:05:00 -0700

    Created ranges include in range-v3 that includes all.hpp

--- en_manual/squareSum10.cpp
+++ en_manual/squareSum10.cpp
@@ -1,13 +1,13 @@
 
-#include <range/v3/all.hpp> // get everything
+#include <ranges>
 
 #include "rangeNamespace.hpp"
 #include "printUtil.hpp"
 
+namespace view = std::view;
 
 int main ()
 {
-    namespace view = std::view;
     int sum = rng::accumulate(view::ints(1, rng::unreachable)
                        | view::transform([](int i){return i*i;})
                        | view::take(10), 0);
