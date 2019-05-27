6cdec1cd6718cbbaeeab10e309cff3dc033736db
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-27 10:33:48 -0700

    22_iotaTake.cpp: added

---
+++ examples/22_iotaTake.cpp
@@ -0,0 +1,45 @@
+/*
+ 22_iotaTake.cpp
+
+ */
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+#include "rangeNamespace.hpp"
+
+namespace view = std::view;
+
+
+int main()
+{
+    std::vector<int> vi {0, 1, 2, 3, 4, 5};
+
+    // range.take.overview
+    // take_view produces a View of the first N elements from another View,
+    // or all the elements if the adapted View contains fewer than N.
+    {
+        rng::take_view tv{vi, 2};
+        for (int i : tv) {
+            std::cout << i << " "; //0 1
+        }
+        std::cout << "\n";
+    }
+    {
+        auto tv = rng::take_view{vi, 2}; //construct via assignment
+        for (int i : tv) {
+            std::cout << i << " "; //0 1
+        }
+        std::cout << "\n";
+    }
+    {
+        rng::take_view tv{vi, 10}; //10 is greater then vi.size()
+        for (int i : tv) {
+            std::cout << i << " "; //0 1 2 3 4 5
+        }
+        std::cout << "\n";
+    }
+
+}
+
