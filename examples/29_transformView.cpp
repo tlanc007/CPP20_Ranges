6395a243907cdb31a3723b9a9ebb38be17d11eb6
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-27 11:37:41 -0700

    29_transformView.cpp: dropped in view::all to make it compile

--- examples/29_transformView.cpp
+++ examples/29_transformView.cpp
@@ -9,12 +9,14 @@
 
 #include "rangeNamespace.hpp"
 
+namespace view = std::view;
+
 int main()
 {
     //using cmcstl2 this is successful (compile errors with range v3)
     std::vector<int> vi{ 0, 1, 2, 3, 4, 5 };
 
-    rng::transform_view times_ten{ vi, [](int i) { return i * 10; } };
+    rng::transform_view times_ten { view::all(vi), [](int i) { return i * 10; } };
 
     for (int i : times_ten)
     {
