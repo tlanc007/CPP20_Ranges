ee5dcea0ed4346f4ab49851fd00c448dd1fbaa5f
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-27 10:36:41 -0700

    22_iotaTake.cpp: switched to view:all for output

--- examples/22_iotaTake.cpp
+++ examples/22_iotaTake.cpp
@@ -21,23 +21,17 @@ int main()
     // or all the elements if the adapted View contains fewer than N.
     {
         rng::take_view tv{vi, 2};
-        for (int i : tv) {
-            std::cout << i << " "; //0 1
-        }
+        std::cout << view::all (tv); // [0,1]
         std::cout << "\n";
     }
     {
         auto tv = rng::take_view{vi, 2}; //construct via assignment
-        for (int i : tv) {
-            std::cout << i << " "; //0 1
-        }
+        std::cout << view::all (tv); // [0,1]
         std::cout << "\n";
     }
     {
         rng::take_view tv{vi, 10}; //10 is greater then vi.size()
-        for (int i : tv) {
-            std::cout << i << " "; //0 1 2 3 4 5
-        }
+        std::cout << view::all (tv); // [0,1,2,3,4,5]
         std::cout << "\n";
     }
 
