2f91c43952b09dcf860c102ccd99b39c96ea61cd
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-27 10:44:57 -0700

    23stringJoin.cpp: added

---
+++ examples/23_stringJoin.cpp
@@ -0,0 +1,27 @@
+/*
+ 22_iotaTake.cpp
+
+ */
+
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <vector>
+
+#include "rangeNamespace.hpp"
+
+namespace view = std::view;
+
+
+int main()
+{
+    std::vector<std::string> vs{"hello", " ", "ranges", "!"};
+
+    // range.join.overview join_view flattens a View of
+    // ranges into a View
+    for ( char ch : rng::join_view{vs} ) {
+        std::cout << ch; // hello ranges!
+    }
+    std::cout << '\n';
+
+}
