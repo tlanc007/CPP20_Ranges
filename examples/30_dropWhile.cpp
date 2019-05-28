75ff49ed305e316b8d0a2c2d425aedc25426823c
Tal Lancaster <tal.lancaster+gh@gmail.com>
2019-05-27 17:00:26 -0700

    30_dropWhile.cpp: original; doesn't compile with range-v3

---
+++ examples/30_dropWhile.cpp
@@ -0,0 +1,29 @@
+/*
+ 30_dropWhile.cpp
+
+ */
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+#include "rangeNamespace.hpp"
+
+namespace view = std::view;
+
+int main()
+{
+    std::vector<int> v { 6, 2, 3, 4, 5, 6 , 7 };
+
+    auto is_even = [](int i) { return i % 2 == 0; };
+    auto print   = [](int i) { std::cout << i << " "; };
+
+    auto after_leading_evens =
+        view::drop_while(v, is_even) |
+        view::take(2);
+
+    //execute
+    rng::for_each(after_leading_evens, print);  //prints-> 3 4
+
+    std::cout << "\n";
+}
